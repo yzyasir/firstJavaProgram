23bb4399f6defc63396daf38cdc9006d5535615d
Yasir Zafar <yzyasir@yahoo.com>
2020-12-05 16:15:50 -0500

    Learning how to run .class files in Java

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Yasir Zafar");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Lorton, VA");
+    }
+}
